7a3cf89eec085de51faa1297f7eee0ec13f6baba
Christoffersenry <rcvb8924@gmail.com>
2022-04-01 09:22:25 +0000

    Clone

---
+++ index.js
@@ -0,0 +1,22 @@
+const   http = require("http"),
+        logger = require("morgan"),
+        express = require("express"),
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose");
+
+let app = express();
+let port = 8000;
+
+app.use(bodyParser.json());
+app.use(require('./routes'));
+app.use(logger("tiny"));
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
+
+app.listen(port, function(err){
+    console.log("Listening on port: " + port)
+});
